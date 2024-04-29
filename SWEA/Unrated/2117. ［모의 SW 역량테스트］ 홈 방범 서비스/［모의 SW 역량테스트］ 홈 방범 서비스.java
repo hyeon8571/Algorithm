742e6e4d48e5964dc0d78558b8cdeb8f05b423c1
seunghyeon <97227876+hyeon8571@users.noreply.github.com>
2024-04-29 19:37:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 372 ms, Memory: 93,904 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -6,17 +6,21 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
-public class Solution{
+public class Solution {
 
     static class Place {
-        int y, x, level;
+        int y, x, k;
 
-        public Place (int y, int x, int level) {
+        public Place(int y, int x, int k) {
             this.y = y;
             this.x = x;
-            this.level = level;
+            this.k = k;
         }
     }
+
+    static int[] dx = new int[] {0, 1, 0, -1};
+
+    static int[] dy = new int[] {-1, 0, 1, 0};
 
     static int N, M;
 
@@ -26,26 +30,21 @@ public class Solution{
 
     static int result;
 
-    static int[] dx = new int[] {0, 1, 0, -1};
-
-    static int[] dy = new int[] {-1, 0, 1, 0};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int tc = Integer.parseInt(br.readLine());
 
         for (int t = 1; t <= tc; t++) {
-
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             N = Integer.parseInt(st.nextToken());
 
             M = Integer.parseInt(st.nextToken());
 
-            grid = new int[N][N];
-
             result = 0;
+
+            grid = new int[N][N];
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -57,7 +56,7 @@ public class Solution{
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     visited = new boolean[N][N];
-                    homeservice(i, j);
+                    bfs(i, j);
                 }
             }
 
@@ -66,69 +65,54 @@ public class Solution{
         }
     }
 
-    public static void homeservice(int startY, int startX) {
+    public static void bfs(int startY, int startX) {
         ArrayDeque<Place> queue = new ArrayDeque<>();
-
-        visited[startY][startX] = true;
 
         queue.add(new Place(startY, startX, 1));
 
-        int profit =  0;
+        visited[startY][startX] = true;
+
+        int k = 1;
+
+        int home = 0;
 
         if (grid[startY][startX] == 1) {
-            profit = M - 1;
-        } else {
-            profit = -1;
+            home++;
         }
 
-        if (profit >= 0) {
-            if (result < 1) {
-                result = 1;
-            }
-        }
+        int profit = M * home - 1;
 
-        int distance = 1;
+        if (profit >= 0 && home > result) {
+            result = home;
+        }
 
         while (!queue.isEmpty()) {
             Place now = queue.pollFirst();
 
-            if (now.level > distance) {
-                distance = now.level;
+            if (now.k > k) {
+                k = now.k;
+                profit = M*home - (k*k + (k-1)*(k-1));
 
-                int cnt = 0;
-
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        if (visited[i][j] && grid[i][j] == 1) {
-                            cnt++;
-                        }
-                    }
+                if (profit >= 0 && home > result) {
+                    result = home;
                 }
-
-                profit = M*cnt - (distance * distance + (distance - 1) * (distance - 1));
-
-                if (profit >= 0) {
-                    if (cnt > result) {
-                        result = cnt;
-                    }
-                }
-
             }
 
             for (int i = 0; i < 4; i++) {
+                int ny = now.y + dy[i];
                 int nx = now.x + dx[i];
 
-                int ny = now.y + dy[i];
-
-                if (0 <= nx && nx < N && 0 <= ny && ny < N) {
-
+                if (0 <= ny && ny < N && 0 <= nx && nx < N) {
                     if (!visited[ny][nx]) {
                         visited[ny][nx] = true;
-                        queue.add(new Place(ny, nx, now.level + 1));
+                        queue.add(new Place(ny, nx, now.k + 1));
+
+                        if (grid[ny][nx] == 1) {
+                            home++;
+                        }
                     }
                 }
             }
         }
-
     }
 }
