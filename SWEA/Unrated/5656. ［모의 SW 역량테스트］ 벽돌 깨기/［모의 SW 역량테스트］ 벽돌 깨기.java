e2391922f6bbce545348641fabdaf4e139ea45c3
seunghyeon <97227876+hyeon8571@users.noreply.github.com>
2024-05-03 14:58:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 1,749 ms, Memory: 118,228 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +6,7 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	
-	public static class Place {
+	static class Place {
 		int y, x;
 		
 		public Place(int y, int x) {
@@ -19,17 +17,17 @@ public class Solution {
 	
 	static int N, W, H;
 	
-	static int[][] grid, tmp;
+	static int[][] grid;
 	
 	static int[] arr;
+	
+	static int[][] temp;
+
+	static int result;
 	
 	static int[] dx = new int[] {0, 1, 0, -1};
 	
 	static int[] dy = new int[] {-1, 0, 1, 0};
-	
-	static int cnt;
-	
-	static int result;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -40,10 +38,12 @@ public class Solution {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
 			N = Integer.parseInt(st.nextToken());
-			
 			W = Integer.parseInt(st.nextToken());
-			
 			H = Integer.parseInt(st.nextToken());
+			
+			result = Integer.MAX_VALUE;
+			
+			arr = new int[N];
 			
 			grid = new int[H][W];
 			
@@ -54,118 +54,110 @@ public class Solution {
 				}
 			}
 			
-			arr = new int[N];
-			
-			cnt = 0;
-			
-			result = Integer.MAX_VALUE;
-		
-			choice(0);
+			dupPermutation(0);
 			
 			System.out.println("#" + t + " " + result);
-			
 		}
 	}
 	
-	// 중복순열 (W 중에서 N개 뽑기)
-	public static void choice(int depth) {
+	// 중복순열로 폭탄 위치 설정
+	public static void dupPermutation(int depth) {
 		
 		if (depth == N) {
-			test(arr);
+
+			bomb(arr);
+			
 			return;
 		}
 		
 		for (int i = 0; i < W; i++) {
 			arr[depth] = i;
-			choice(depth + 1);
+			dupPermutation(depth + 1);
 		}
 	}
 	
-	public static void test(int[] order) {
-		
-		tmp = new int[H][W];
+	public static void bomb(int[] arr) {
+		temp = new int[H][W];
 		
 		for (int i = 0; i < H; i++) {
 			for (int j = 0; j < W; j++) {
-				tmp[i][j] = grid[i][j];
+				temp[i][j] = grid[i][j];
 			}
 		}
 		
-		for (int k = 0; k < order.length; k++) { 
-			
-			for (int i = 0; i < H; i++) {
-				if (tmp[i][order[k]] != 0) {
-					
-					boom(i, order[k]);
-					
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < H; j++) {
+				
+				if (temp[j][arr[i]] != 0) {
+					pung(j, arr[i]);
 					break;
-		
 				}
 			}
-			
 		}
 		
-		int count = 0;
 		
+		int cnt = 0;
 		for (int i = 0; i < H; i++) {
 			for (int j = 0; j < W; j++) {
-				if (tmp[i][j] != 0) {
-					count++;
+				if (temp[i][j] != 0) {
+					cnt++;
 				}
 			}
 		}
 		
-		if (count < result) {
-			result = count;
+		if (result > cnt) {
+			result = cnt;
 		}
+		
+		
 	}
 	
-	public static void boom(int startY, int startX) {
-		
-		
+	public static void pung(int startY, int startX) {
 		ArrayDeque<Place> queue = new ArrayDeque<>();
 		
 		queue.add(new Place(startY, startX));
 		
-		while(!queue.isEmpty()) {
-			
+		while (!queue.isEmpty()) {
 			Place now = queue.pollFirst();
 			
-			int range = tmp[now.y][now.x];
+			int range = temp[now.y][now.x];
 			
-			tmp[now.y][now.x] = 0;
+			temp[now.y][now.x] = 0;
 			
 			for (int i = 0; i < 4; i++) {
 				for (int j = 1; j < range; j++) {
-					int nx = now.x + dx[i] * j;
 					int ny = now.y + dy[i] * j;
+					int nx = now.x + dx[i] * j;
 					
-					if (0 <= nx && nx < W && 0 <= ny && ny < H) {
-						if (tmp[ny][nx] != 0) {
+					if (0 <= ny && ny < H && 0 <= nx && nx < W) {
+						if (temp[ny][nx] != 0) {
 							queue.add(new Place(ny, nx));
 						}
+						
 					}
 				}
+				
+				
 			}
-		}	
-		
+			
+		}
 		down();
 		
 	}
 	
 	public static void down() {
+		
 		for (int i = 0; i < W; i++) {
 			for (int j = 0; j < H - 1; j++) {
-				if (tmp[j][i] != 0) {
-					if (tmp[j+1][i] == 0) {
-						tmp[j+1][i] = tmp[j][i];
-						tmp[j][i] = 0;
+				if (temp[j][i] != 0) {
+					if (temp[j + 1][i] == 0) {
+						temp[j+1][i] = temp[j][i];
+						temp[j][i] = 0;
 						down();
 					}
 				}
 			}
 		}
-		
 	}
 	
 }
