0d9c25e81813f396266f2cd2146d543515cf0b0f
hyeon8571 <97227876+hyeon8571@users.noreply.github.com>
2024-09-04 06:37:37 +0900

    [Silver III] Title: N과 M (3), Time: 516 ms, Memory: 208216 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+
+    static int[] result;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        M = Integer.parseInt(st.nextToken());
+
+        result = new int[M];
+
+        // 중복순열
+        duplePermutation(0);
+
+        System.out.println(sb);
+
+    }
+
+    public static void duplePermutation(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            result[depth] = i;
+            duplePermutation(depth + 1);
+        }
+    }
+}
