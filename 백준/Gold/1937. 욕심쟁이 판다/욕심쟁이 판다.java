979defcee1d9d852237718019aaa77b720e27c82
원승현 <97227876+hyeon8571@users.noreply.github.com>
2025-03-27 23:47:17 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 452 ms, Memory: 37500 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -24,7 +24,6 @@ public class Main {
             }
         }
 
-        // 모든 위치에서 DFS 수행하여 최대 이동 거리 찾기
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 result = Math.max(result, dfs(i, j));
@@ -35,12 +34,11 @@ public class Main {
     }
 
     public static int dfs(int y, int x) {
-        // 이미 방문한 곳이면 저장된 값 반환
         if (dp[y][x] != 0) {
             return dp[y][x];
         }
 
-        dp[y][x] = 1; // 최소한 자기 자신 포함
+        dp[y][x] = 1;
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
@@ -49,6 +47,7 @@ public class Main {
                 if (grid[y][x] < grid[ny][nx]) {
                     dp[y][x] = Math.max(dp[y][x], dfs(ny, nx) + 1);
                 }
+
             }
         }
 
