1e058f61b71bb4bc4ba514e63105b31cad278e8a
원승현 <97227876+hyeon8571@users.noreply.github.com>
2025-06-03 12:31:32 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 16760 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,72 +1,70 @@
+
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, K;
+    static int n;
+    static int k;
 
-    static int[] dx = new int[] {-1, 1};
+    static boolean[] visited;
+    static int[] distance;
 
-    static int[] grid;
-
-    static int[] visited;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        grid = new int[100001];
-        visited = new int[100001];
+        visited = new boolean[100001];
+        distance = new int[100001];
 
-        bfs(N);
+        bfs(n, k);
 
-        System.out.println(visited[K] - 1);
+        System.out.println(distance[k]);
     }
 
-    public static void bfs(int start) {
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
-
-        visited[start] = 1;
+    public static void bfs(int start, int end) {
+        Queue<Integer> queue = new ArrayDeque<>();
         queue.add(start);
+        visited[start] = true;
 
         while (!queue.isEmpty()) {
-            Integer now = queue.pollFirst();
+            int now = queue.poll();
 
-            for (int i = 0; i < 3; i++) {
-                int nx = now;
-                if (i == 2) {
-                    nx = now + now;
-                } else {
-                    nx = now + dx[i];
+            // +1
+            int nx = now + 1;
+            if (0 <= nx && nx < 100001) {
+                if (!visited[nx] || distance[nx] > distance[now] + 1) {
+                    distance[nx] = distance[now] + 1;
+                    visited[nx] = true;
+                    queue.add(nx);
                 }
+            }
 
-                if (0  <= nx && nx <= 100000) {
-                    if (visited[nx] == 0 && i != 2)  {
-                        visited[nx] = visited[now] + 1;
-                        queue.add(nx);
-                    } else if (visited[nx] != 0 && i != 2) {
-                        if (visited[nx] > visited[now] + 1) {
-                            visited[nx] = visited[now] + 1;
-                            queue.add(nx);
-                        }
-                    } else if (visited[nx] == 0 && i == 2) {
-                        visited[nx] = visited[now];
-                        queue.add(nx);
-                    } else if (visited[nx] != 0 && i == 2) {
-                        if (visited[nx] > visited[now]) {
-                            visited[nx] = visited[now];
-                            queue.add(nx);
-                        }
-                    }
+            // -1
+            nx = now - 1;
+            if (0 <= nx && nx < 100001) {
+                if (!visited[nx] || distance[nx] > distance[now] + 1) {
+                    distance[nx] = distance[now] + 1;
+                    visited[nx] = true;
+                    queue.add(nx);
                 }
+            }
 
+            // *2
+            nx = now * 2;
+            if (0 <= nx && nx < 100001) {
+                if (!visited[nx] || distance[nx] > distance[now]) {
+                    distance[nx] = distance[now];
+                    visited[nx] = true;
+                    queue.add(nx);
+                }
             }
         }
     }
