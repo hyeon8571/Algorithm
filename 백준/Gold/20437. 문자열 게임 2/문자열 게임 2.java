9c5090920fefc15a534702045ad85e175ea82f94
원승현 <97227876+hyeon8571@users.noreply.github.com>
2025-07-03 01:08:09 +0900

    [Gold V] Title: 문자열 게임 2, Time: 396 ms, Memory: 47748 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -4,41 +4,45 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int T = Integer.parseInt(br.readLine());
         
-        for (int i = 0; i < T; i++) {
-            String W = br.readLine();
-            int K = Integer.parseInt(br.readLine());
+        for(int t=0; t<T; t++) {
+            String s = br.readLine();
+            int k = Integer.parseInt(br.readLine());
             
-            if(K == 1) {
-                System.out.println("1 1");
-                continue;
+            Map<Character, List<Integer>> alpha = new HashMap<>();
+            
+            for(int i=0; i<s.length(); i++) {
+                List<Integer> list = alpha.getOrDefault(s.charAt(i), new ArrayList<>());
+                list.add(i);
+                alpha.put(s.charAt(i), list);
             }
             
-            int[] alpha = new int[26];
-            for (int j = 0; j < W.length(); j++) {
-                alpha[W.charAt(j) - 'a']++;
-            }
+            int minLen = Integer.MAX_VALUE;
+            int maxLen = Integer.MIN_VALUE;
             
-            
-            int min = Integer.MAX_VALUE;
-            int max = -1;
-            for (int j = 0; j < W.length(); j++) {
-                if(alpha[W.charAt(j) - 'a'] < K) continue;
+            for(char key: alpha.keySet()) {
+                List<Integer> list = alpha.get(key);
+                if(list.size() < k) continue;
                 
-                int count = 1;
-                for (int l = j+1; l < W.length(); l++) {
-                    if (W.charAt(j) == W.charAt(l)) count++;
-                    if (count == K) {
-                        min = Math.min(min, l - j + 1);
-                        max = Math.max(max, l - j + 1);
-                        break;
-                    }
+                for(int i=0; i<=list.size()-k; i++) {
+                    int start = list.get(i);
+                    int end = list.get(i+k-1);
+                    int len = end - start + 1;
+                    
+                    minLen = Math.min(len, minLen);
+                    maxLen = Math.max(len, maxLen);
                 }
             }
-            if(min == Integer.MAX_VALUE || max == -1) System.out.println("-1");
-            else System.out.println(min + " " + max);
+            
+            if(minLen==Integer.MAX_VALUE || maxLen==Integer.MIN_VALUE) {
+                System.out.println(-1);
+            } else {
+                System.out.println(minLen + " " + maxLen);
+            }
         }
+    
     }
 }
+
+
