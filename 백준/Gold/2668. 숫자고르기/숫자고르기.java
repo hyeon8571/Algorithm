127fcdd9359bb3e34cd0196292449e52022dddeb
원승현 <97227876+hyeon8571@users.noreply.github.com>
2025-07-11 17:43:54 +0900

    [Gold V] Title: 숫자고르기, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -2,55 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
+    public static boolean[] visited;
+    public static int[] num;
+    public static List<Integer> list;
+    
     public static void main(String[] args) throws Exception {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
         int N = Integer.parseInt(br.readLine());
         
-        List<Integer> list = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-                
+        num = new int[N+1];
+        visited = new boolean[N+1];
+        
         for (int i = 1; i <= N; i++) {
-            list.add(i);
-            list2.add(Integer.parseInt(br.readLine()));
+            num[i] = Integer.parseInt(br.readLine());
         }
+        
+        list = new ArrayList<>();
+        
+        for (int i = 1; i <= N; i++) {
+            visited[i] = true;
+            dfs(i, i);
+            visited[i] = false;
+        }
+        
+        Collections.sort(list);
+        
+        System.out.println(list.size());
+        
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        
+    }
     
-        while (list.size() > 0) {
-            for (int i = 0; i < list.size(); i++) {
-                int val = list.get(i);
-                boolean flag = false;
-                for (int j = 0; j < list2.size(); j++) {
-                    if (list2.get(j) == val) {
-                        flag = true;
-                    }
-                }
-                
-                if (!flag) {
-                    list.remove(i);
-                    list2.remove(i);
-                } else {
-                    HashSet<Integer> set1 = new HashSet<>();
-                    HashSet<Integer> set2 = new HashSet<>();
-                    
-                    for (int k = 0; k < list.size(); k++) {
-                        set1.add(list.get(k));
-                    }
-                    
-                    for (int l = 0; l < list2.size(); l++) {
-                        set2.add(list2.get(l));
-                    }
-                    
-                    if (set1.size() == set2.size()) {
-                        System.out.println(set1.size());
-                        for (int y = 0; y < list.size(); y++) {
-                            System.out.println(list.get(y));
-                        }
-                        
-                        return;
-                    }
-                }
+    public static void dfs(int start, int target) {
+        
+        int next = num[start];
+        
+        if (!visited[next]) {
+            visited[next] = true;
+            dfs(next, target);
+            visited[next] = false;
+        } else {
+            if (target == next) {
+                list.add(target);
+                return;
             }
         }
-    
     }
 }
