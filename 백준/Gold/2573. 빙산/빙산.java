04377a148fae5845599a8355b9fb7fb2f87f8ca5
원승현 <97227876+hyeon8571@users.noreply.github.com>
2025-09-23 22:13:35 +0900

    [Gold IV] Title: 빙산, Time: 648 ms, Memory: 221784 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,40 +1,26 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static class Place {
-        int x, y;
-
-        public Place (int y, int x) {
+        int y, x, near;
+        public Place(int y, int x, int near) {
             this.y = y;
             this.x = x;
+            this.near = near;
         }
     }
 
-    static int N, M;
+    public static int[][] grid;
+    public static int N, M;
+    public static boolean[][] visited;
 
-    static int[][] grid;
-
-    static int[][] meltCnt;
-
-    static boolean[][] visited;
-
-    static int[] dx = new int[] {0, 1, 0, -1};
-
-    static int[] dy = new int[] {-1, 0, 1, 0};
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
-
         M = Integer.parseInt(st.nextToken());
 
         grid = new int[N][M];
@@ -46,121 +32,112 @@ public class Main {
             }
         }
 
-        int time = 0;
+        int result = 0;
         while (true) {
-
-            int cnt = 0;
-
-            int check = 0;
-
             visited = new boolean[N][M];
+            int cnt = 0; // 덩어리 개수
 
-            time++;
-
-            ice();
-            
-
+            int zeroCheck = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
                     if (grid[i][j] != 0) {
-                        check++;
+                        zeroCheck++;
                     }
                 }
             }
 
-            if (check == 0) {
+            if (zeroCheck == 0) {
                 System.out.println(0);
                 return;
             }
 
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    if (!visited[i][j] && grid[i][j] != 0) {
+                    if (grid[i][j] != 0 && !visited[i][j]) {
+                        visited[i][j] = true;
                         bfs(i, j);
                         cnt++;
                     }
-                    if (cnt >= 2) {
-                        System.out.println(time);
-                        return;
-                    }
                 }
             }
 
+            if (cnt >= 2) {
+                break;
+            }
 
-
-
+            melt();
+            result++;
 
         }
 
+        System.out.println(result);
+
     }
 
-    public static void bfs(int startY, int startX) {
-        ArrayDeque<Place> queue = new ArrayDeque<>();
 
-        queue.add(new Place(startY, startX));
+    public static void bfs(int y, int x) {
+        int[] dx = new int[] {0, 1, 0, -1};
+        int[] dy = new int[] {-1, 0, 1, 0};
+        Queue<Place> queue = new ArrayDeque();
+        queue.add(new Place(y, x, 0));
 
-        visited[startY][startX] = true;
-
-        while(!queue.isEmpty()) {
-
-            Place now = queue.pollFirst();
+        while (!queue.isEmpty()) {
+            Place now = queue.poll();
 
             for (int i = 0; i < 4; i++) {
-
+                int ny = now.y + dy[i];
                 int nx = now.x + dx[i];
 
-                int ny = now.y + dy[i];
-
                 if (0 <= nx && nx < M && 0 <= ny && ny < N) {
-                    if (!visited[ny][nx] && grid[ny][nx] != 0) {
+                    if (grid[ny][nx] != 0 && !visited[ny][nx]) {
                         visited[ny][nx] = true;
-                        queue.add(new Place(ny, nx));
+                        queue.add(new Place(ny, nx, 0));
                     }
                 }
             }
         }
     }
 
-    public static void ice() {
-        meltCnt = new int[N][M];
+    public static void melt() {
+        Queue<Place> queue = new ArrayDeque<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (grid[i][j] != 0) {
-                    countWater(i, j);
+                    int near = check(i, j);
+                    queue.add(new Place(i, j, near));
                 }
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (grid[i][j] - meltCnt[i][j] < 0) {
-                    grid[i][j] = 0;
-                } else {
-                    grid[i][j] -= meltCnt[i][j];
-                }
+        while(!queue.isEmpty()) {
+            Place now = queue.poll();
+            grid[now.y][now.x] -= now.near;
+            
+            if (grid[now.y][now.x] < 0) {
+                grid[now.y][now.x] = 0;
             }
         }
 
     }
 
-    public static void countWater(int y, int x) {
+    public static int check(int y, int x) {
+        int[] dx = new int[] {0, 1, 0, -1};
+        int[] dy = new int[] {-1, 0, 1, 0};
 
-
-        int cnt = 0;
+        int result = 0;
 
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
             int ny = y + dy[i];
+            int nx = x + dx[i];
 
-            if (grid[ny][nx] == 0) {
-                cnt++;
+            if (0 <= nx && nx < M && 0 <= ny && ny < N) {
+                if (grid[ny][nx] == 0) {
+                    result++;
+                }
             }
-
         }
 
-        meltCnt[y][x] = cnt;
-
+        return result;
     }
-
 }
