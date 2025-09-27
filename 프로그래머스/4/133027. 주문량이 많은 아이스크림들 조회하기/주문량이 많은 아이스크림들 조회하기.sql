4faadf9f36f9056f622b910425bc2205093b1845
원승현 <97227876+hyeon8571@users.noreply.github.com>
2025-09-28 01:02:04 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,21 +1,24 @@
--- 7월 아이스크림 총 주문량과 상반기의 아이스크림 총 주문량을 더한 값이 큰 순서대로 상위 3개의 맛을 조회하는 SQL 문을 작성해주세요.
-
-SELECT T.FLAVOR
+-- 7월 아이스크림 총 주문량과 상반기의 아이스크림 총 주문량을 더한 값이 큰 순서대로 상위 3개의 맛을 조회하는 SQL 
+SELECT FLAVOR
 FROM 
     (
-        SELECT A.FLAVOR, A_TOTAL + B_TOTAL
+        SELECT FF.FLAVOR, FF.TOTAL_ORDER + TT.TOTAL_ORDER AS TOTAL_ORDER
         FROM 
             (
-                SELECT FLAVOR, SUM(TOTAL_ORDER) AS A_TOTAL
-                FROM FIRST_HALF
+                SELECT FLAVOR, SUM(TOTAL_ORDER) TOTAL_ORDER
+                FROM 
+                    FIRST_HALF
                 GROUP BY FLAVOR
-            ) A JOIN
+            ) FF JOIN
             (
-                SELECT FLAVOR, SUM(TOTAL_ORDER) AS B_TOTAL
-                FROM JULY
+                SELECT FLAVOR, SUM(TOTAL_ORDER) TOTAL_ORDER
+                FROM
+                    JULY
                 GROUP BY FLAVOR
-            ) B ON A.FLAVOR = B.FLAVOR
-        ORDER BY A_TOTAL + B_TOTAL DESC
-    ) T
+            ) TT
+            ON FF.FLAVOR = TT.FLAVOR
+        ORDER BY TOTAL_ORDER DESC
+    )
 WHERE ROWNUM <= 3
+    
 
