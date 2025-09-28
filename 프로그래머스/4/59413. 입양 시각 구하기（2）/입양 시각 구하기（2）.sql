62334ea2bf611ec96f59f2356c9bf23326e1fb4f
원승현 <97227876+hyeon8571@users.noreply.github.com>
2025-09-29 00:35:07 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,10 +1,12 @@
--- 0시부터 23시까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문
-SELECT h.hour,
-       NVL(COUNT(a.animal_id), 0) AS cnt
-FROM   (SELECT LEVEL - 1 AS hour
-        FROM dual
-        CONNECT BY LEVEL <= 24) h
-       LEFT JOIN animal_outs a
-              ON TO_NUMBER(TO_CHAR(a.datetime, 'HH24')) = h.hour
-GROUP  BY h.hour
-ORDER  BY h.hour;
+-- 0시부터 23시까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요. 
+SELECT H.HOUR AS HOUR, NVL(COUNT(AO.ANIMAL_ID), 0) AS COUNT
+FROM 
+    ANIMAL_OUTS AO
+RIGHT JOIN (
+    SELECT LEVEL - 1 AS HOUR
+    FROM DUAL
+    CONNECT BY LEVEL <= 24
+) H
+ON TO_NUMBER(TO_CHAR(AO.DATETIME, 'HH24')) = H.HOUR
+GROUP BY H.HOUR
+ORDER BY HOUR
